b304f9c78fa3efb4970de16f88671a43559e1e8b
Phong <p7157568@gmail.com>
2016-07-09 23:33:36 +0700

    feat - add toolbar with hamberger icon

--- app/src/main/java/dhbk/android/todomvp/MainActivity.java
+++ app/src/main/java/dhbk/android/todomvp/MainActivity.java
@@ -1,7 +1,9 @@
 package dhbk.android.todomvp;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up the toolbar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
     }
 
 }
