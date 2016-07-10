61a92a6457a565dfa9fe786897847d9e331a942f
Phong <p7157568@gmail.com>
2016-07-10 10:49:20 +0700

    feat - create the presenter class

---
+++ app/src/main/java/dhbk/android/todomvp/TasksPresenter.java
@@ -0,0 +1,58 @@
+package dhbk.android.todomvp;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Created by huynhducthanhphong on 7/10/16.
+ */
+public class TasksPresenter implements TasksContract.Presenter {
+    @Override
+    public void openTaskDetails(@NonNull Task requestedTask) {
+
+    }
+
+    @Override
+    public void completeTask(@NonNull Task completedTask) {
+
+    }
+
+    @Override
+    public void activateTask(@NonNull Task activeTask) {
+
+    }
+
+    @Override
+    public void result(int requestCode, int resultCode) {
+
+    }
+
+    @Override
+    public void loadTasks(boolean forceUpdate) {
+
+    }
+
+    @Override
+    public void addNewTask() {
+
+    }
+
+    @Override
+    public void clearCompletedTasks() {
+
+    }
+
+    @Override
+    public void setFiltering(TasksFilterType requestType) {
+
+    }
+
+    @Override
+    public TasksFilterType getFiltering() {
+        return null;
+    }
+
+    @Override
+    public void start() {
+
+    }
+}
