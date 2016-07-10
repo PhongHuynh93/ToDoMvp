cbdb75f86c66c91114119f0d87f7262cf2cf226d
Phong <p7157568@gmail.com>
2016-07-10 08:47:58 +0700

    feat - add parent baseView interface, this class contains 1 methods to make view set the presenter

---
+++ app/src/main/java/dhbk/android/todomvp/BaseView.java
@@ -0,0 +1,11 @@
+package dhbk.android.todomvp;
+
+/**
+ * Created by huynhducthanhphong on 7/10/16.
+ */
+
+// this parent class to set the presenter to fragment
+public interface BaseView<T> {
+    void setPresenter(T presenter);
+
+}
