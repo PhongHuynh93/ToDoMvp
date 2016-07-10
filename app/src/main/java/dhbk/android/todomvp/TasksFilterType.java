df62d10935c34833cbc4a9e720674eb597a37d36
Phong <p7157568@gmail.com>
2016-07-10 08:27:09 +0700

    feat - add enum class, 3 states to present the state of tasks

---
+++ app/src/main/java/dhbk/android/todomvp/TasksFilterType.java
@@ -0,0 +1,21 @@
+package dhbk.android.todomvp;
+
+/**
+ * Used with the filter spinner in the tasks list.
+ */
+public enum TasksFilterType {
+    /**
+     * Do not filter tasks.
+     */
+    ALL_TASKS,
+
+    /**
+     * Filters only the active (not completed yet) tasks.
+     */
+    ACTIVE_TASKS,
+
+    /**
+     * Filters only the completed tasks.
+     */
+    COMPLETED_TASKS
+}
