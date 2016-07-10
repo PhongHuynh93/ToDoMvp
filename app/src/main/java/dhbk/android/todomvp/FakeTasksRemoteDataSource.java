d934d3d60dc65f399c79714ee3ff7bd0ce1393b6
Phong <p7157568@gmail.com>
2016-07-10 11:00:01 +0700

    feat - implement methods in fake

--- app/src/main/java/dhbk/android/todomvp/FakeTasksRemoteDataSource.java
+++ app/src/main/java/dhbk/android/todomvp/FakeTasksRemoteDataSource.java
@@ -4,6 +4,8 @@ package dhbk.android.todomvp;
  * Created by huynhducthanhphong on 7/10/16.
  */
 
+import android.support.annotation.NonNull;
+
 /**
  * Implementation of a remote data source with static access to the data for easy testing.
  */
@@ -21,4 +23,59 @@ public class FakeTasksRemoteDataSource implements TasksDataSource {
     }
 
 
+
+    @Override
+    public void getTasks(@NonNull LoadTasksCallback callback) {
+
+    }
+
+    @Override
+    public void getTask(@NonNull String taskId, @NonNull GetTaskCallback callback) {
+
+    }
+
+    @Override
+    public void saveTask(@NonNull Task task) {
+
+    }
+
+    @Override
+    public void completeTask(@NonNull Task task) {
+
+    }
+
+    @Override
+    public void completeTask(@NonNull String taskId) {
+
+    }
+
+    @Override
+    public void activateTask(@NonNull Task task) {
+
+    }
+
+    @Override
+    public void activateTask(@NonNull String taskId) {
+
+    }
+
+    @Override
+    public void clearCompletedTasks() {
+
+    }
+
+    @Override
+    public void refreshTasks() {
+
+    }
+
+    @Override
+    public void deleteAllTasks() {
+
+    }
+
+    @Override
+    public void deleteTask(@NonNull String taskId) {
+
+    }
 }
