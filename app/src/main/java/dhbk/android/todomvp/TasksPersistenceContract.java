36d5d277bbb9bb84d4956104243e525117950368
Phong <p7157568@gmail.com>
2016-07-10 11:41:04 +0700

    feat - add constan contract class

---
+++ app/src/main/java/dhbk/android/todomvp/TasksPersistenceContract.java
@@ -0,0 +1,26 @@
+package dhbk.android.todomvp;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by huynhducthanhphong on 7/10/16.
+ */
+
+/**
+ * The contract used for the db to save the tasks locally.
+ */
+public final class TasksPersistenceContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public TasksPersistenceContract() {}
+
+    /* Inner class that defines the table contents */
+    public static abstract class TaskEntry implements BaseColumns {
+        public static final String TABLE_NAME = "task";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_DESCRIPTION = "description";
+        public static final String COLUMN_NAME_COMPLETED = "completed";
+    }
+}
