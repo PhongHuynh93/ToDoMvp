204c0b50929bdee77c23bce9062e74f8cc57ae75
Phong <p7157568@gmail.com>
2016-07-10 12:22:44 +0700

    feat - add method to load db in the first time open the app

--- app/src/main/java/dhbk/android/todomvp/tasks/TasksPresenter.java
+++ app/src/main/java/dhbk/android/todomvp/tasks/TasksPresenter.java
@@ -2,7 +2,11 @@ package dhbk.android.todomvp.tasks;
 
 import android.support.annotation.NonNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import dhbk.android.todomvp.data.Task;
+import dhbk.android.todomvp.data.source.TasksDataSource;
 import dhbk.android.todomvp.data.source.TasksRepository;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -12,9 +16,12 @@ import static com.google.common.base.Preconditions.checkNotNull;
  */
 public class TasksPresenter implements TasksContract.Presenter {
 
-
     private final TasksRepository mTasksRepository;
     private final TasksContract.View mTasksView;
+
+    private boolean mFirstLoad = true;
+    private TasksFilterType mCurrentFiltering = TasksFilterType.ALL_TASKS; // load all tasks at the first time.
+
 
     // a model and a view (taskfragment)
     public TasksPresenter(@NonNull TasksRepository tasksRepository, @NonNull TasksContract.View tasksView) {
@@ -44,9 +51,12 @@ public class TasksPresenter implements TasksContract.Presenter {
 
     }
 
+    // connect to db to load task from network
     @Override
     public void loadTasks(boolean forceUpdate) {
-
+        // Simplification for sample: a network reload will be forced on first load.
+        loadTasks(forceUpdate || mFirstLoad, true);
+        mFirstLoad = false;
     }
 
     @Override
@@ -73,4 +83,121 @@ public class TasksPresenter implements TasksContract.Presenter {
     public void start() {
         loadTasks(false);
     }
+
+
+
+    // connect to db to load task from network
+    /**
+     * @param forceUpdate   Pass in true to refresh the data in the {@link TasksDataSource}
+     * @param showLoadingUI Pass in true to display a loading icon in the UI
+     */
+    private void loadTasks(boolean forceUpdate, final boolean showLoadingUI) {
+        if (showLoadingUI) {
+            mTasksView.setLoadingIndicator(true);
+        }
+        if (forceUpdate) {
+            mTasksRepository.refreshTasks();
+        }
+
+//        // The network request might be handled in a different thread so make sure Espresso knows
+//        // that the app is busy until the response is handled.
+//        EspressoIdlingResource.increment(); // App is busy until further notice
+
+        mTasksRepository.getTasks(new TasksDataSource.LoadTasksCallback() {
+            @Override
+            public void onTasksLoaded(List<Task> tasks) {
+                List<Task> tasksToShow = new ArrayList<Task>();
+//
+//                // This callback may be called twice, once for the cache and once for loading
+//                // the data from the server API, so we check before decrementing, otherwise
+//                // it throws "Counter has been corrupted!" exception.
+//                if (!EspressoIdlingResource.getIdlingResource().isIdleNow()) {
+//                    EspressoIdlingResource.decrement(); // Set app as idle.
+//                }
+
+                // We filter the tasks based on the requestType
+                for (Task task : tasks) {
+                    switch (mCurrentFiltering) {
+                        case ALL_TASKS:
+                            tasksToShow.add(task);
+                            break;
+                        case ACTIVE_TASKS:
+                            if (task.isActive()) {
+                                tasksToShow.add(task);
+                            }
+                            break;
+                        case COMPLETED_TASKS:
+                            if (task.isCompleted()) {
+                                tasksToShow.add(task);
+                            }
+                            break;
+                        default:
+                            tasksToShow.add(task);
+                            break;
+                    }
+                }
+                // The view may not be able to handle UI updates anymore
+                if (!mTasksView.isActive()) {
+                    return;
+                }
+                if (showLoadingUI) {
+                    mTasksView.setLoadingIndicator(false);
+                }
+
+                processTasks(tasksToShow);
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+                // The view may not be able to handle UI updates anymore
+                if (!mTasksView.isActive()) {
+                    return;
+                }
+                mTasksView.showLoadingTasksError();
+            }
+        });
+    }
+
+
+    private void processTasks(List<Task> tasks) {
+        if (tasks.isEmpty()) {
+            // Show a message indicating there are no tasks for that filter type.
+            processEmptyTasks();
+        } else {
+            // Show the list of tasks
+            mTasksView.showTasks(tasks);
+            // Set the filter label's text.
+            showFilterLabel();
+        }
+    }
+
+
+    private void processEmptyTasks() {
+        switch (mCurrentFiltering) {
+            case ACTIVE_TASKS:
+                mTasksView.showNoActiveTasks();
+                break;
+            case COMPLETED_TASKS:
+                mTasksView.showNoCompletedTasks();
+                break;
+            default:
+                mTasksView.showNoTasks();
+                break;
+        }
+    }
+
+    private void showFilterLabel() {
+        switch (mCurrentFiltering) {
+            case ACTIVE_TASKS:
+                mTasksView.showActiveFilterLabel();
+                break;
+            case COMPLETED_TASKS:
+                mTasksView.showCompletedFilterLabel();
+                break;
+            default:
+                mTasksView.showAllFilterLabel();
+                break;
+        }
+    }
+
 }
